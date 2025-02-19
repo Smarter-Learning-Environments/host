9a1e7b820260ab3278240fe9a10153098e07f2e3
dgpenastein <dgpenastein@wpi.edu>
2025-02-18 23:17:00 -0500

    MQTT connection established

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+import paho.mqtt.client as mqtt
 
 app = FastAPI()
 
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.connect("mqtt-broker", 1883, 60)
+
+
 @app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.post("/")
 def read_root():
     return {"message": "Hello, World!"}
